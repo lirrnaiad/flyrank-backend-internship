7ba5c50be5d25d7336a05a534293f55068f7eeab
Sean <seanivanfabia@gmail.com>
2026-09-04 14:00:23 +0800

    Stage 3: create with validation

--- week-2/crud-api/main.py
+++ week-2/crud-api/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
@@ -9,11 +10,20 @@ class Task(BaseModel):
     done: bool = False
 
 
+class TaskCreate(BaseModel):
+    title: str
+
+
+class TaskUpdate(BaseModel):
+    
+
 tasks = [
     Task(id=1, title="Complete report", done=False),
     Task(id=2, title="Send email", done=False),
     Task(id=3, title="Review code", done=False)
 ]
+
+current_free_id = 4
 
 
 @app.get("/")
@@ -22,7 +32,7 @@ def root():
 
 
 @app.get("/health")
-def status():
+def health():
     return {"status": "ok"}
 
 
@@ -41,3 +51,16 @@ def get_task(task_id: int):
         raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
     else:
         return task
+
+
+@app.post("/tasks", status_code=201)
+def add_task(task: TaskCreate):
+    global current_free_id
+
+    if task is None:
+        raise HTTPException(status_code=400, detail="Title is empty")
+
+    new_task = Task(id=current_free_id, title=task.title, done=False)
+    tasks.append(new_task)
+    current_free_id += 1
+    return new_task     
